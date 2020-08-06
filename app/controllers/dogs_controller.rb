3159573d275ad1fb3b8454bdaf7cb06f1f6069e6
iuriseara <iuriseara@gmail.com>
2020-08-06 17:32:51 -0400

    Done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,34 @@
 class DogsController < ApplicationController
+
+    def new
+        @dogs = Dog.new
+    end
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def create
+        @dog = Dog.create(dog_params)
+        redirect_to dog_path(@dog)
+    end
+
+    def edit
+        @dog = dog.find(params[:id])
+    end
+
+    def update
+        @dog = Dog.update(dog_params)
+        redirect_to dog_path(@dog)
+    end
+
+    private
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
+
 end
